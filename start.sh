2d9d27d8c60fe0f3a6b284b597cda0d8e2e8768a
Yo babe <104895694+Jeolpaul2@users.noreply.github.com>
2022-11-29 21:42:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jeolpaul/George-MalarAutofilter.git /George-Malar-Autofilter
+  git clone https://github.com/Jeolpaul2/evamaria /evamaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /George-Malar-Autofilter
+  git clone $UPSTREAM_REPO /evamaria
 fi
-cd /George-Malar-Autofilter
+cd /evamaria
 pip3 install -U -r requirements.txt
 echo "BOT IS STARTING"
 python3 botz.py
