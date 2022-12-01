b5a86456622f181b6cf0e9b586757ef5c47aaa1d
Yo babe <104895694+Jeolpaul2@users.noreply.github.com>
2022-12-01 21:37:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "LEECH")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001545756568))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MR_LINK_Z')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
